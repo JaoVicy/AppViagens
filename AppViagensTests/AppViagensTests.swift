b4f1e04968e5830aba8905637c5c2a642733540b
JaoVicy <filipimjoa@gmail.com>
2025-06-12 16:34:02 -0300

    Initial Commit

---
+++ AppViagensTests/AppViagensTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppViagensTests.swift
+//  AppViagensTests
+//
+//  Created by João Víctor Benetti Filipim on 12/06/25.
+//
+
+import Testing
+@testable import AppViagens
+
+struct AppViagensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

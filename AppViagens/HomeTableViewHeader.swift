ca5f8988a00a88dba97e5a7143eb5f0790f02fc1
JaoVicy <filipimjoa@gmail.com>
2025-06-13 18:52:57 -0300

    Added header - need fix

---
+++ AppViagens/HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  AppViagens
+//
+//  Created by João Víctor Benetti Filipim on 13/06/25.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

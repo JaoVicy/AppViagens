60b3a5090da916bd39d86a2b50616dc2e967a14f
JaoVicy <filipimjoa@gmail.com>
2025-06-20 14:21:15 -0300

    Building Outlets

--- AppViagens/View/Cells/ViagemTableViewCell.swift
+++ AppViagens/View/Cells/ViagemTableViewCell.swift
@@ -19,4 +19,27 @@ class ViagemTableViewCell: UITableViewCell {
     @IBOutlet weak var precoSemDescontoLabel: UILabel!
     @IBOutlet weak var precoViagemLabel: UILabel!
     @IBOutlet weak var statusCancelamentoViagemLabel: UILabel!
+    
+    func configuraCelula(_ viagem: Viagem?) {
+        
+        viagemImage.image = UIImage(named: viagem?.asset ?? "")
+        tituloViagemLabel.text = viagem?.titulo
+        subtituloViagemLabel.text = viagem?.subtitulo
+        precoViagemLabel.text = "R$ \(viagem?.preco ?? 0.00)"
+        
+        let atributoString: NSMutableAttributedString = NSMutableAttributedString(string: "R$ \(viagem?.precoSemDesconto ?? 0)")
+        atributoString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0, atributoString.length))
+        precoSemDescontoLabel.attributedText = atributoString
+        
+        if let numerodeDias = viagem?.diaria, let numeroDeHospedes = viagem?.hospedes {
+            let diarias = numerodeDias == 1 ? "Diária" : "Diárias"
+            let hospedes = numeroDeHospedes == 1 ? "Pessoa" : "Pessoas"
+            
+            diariaViagemLabel.text = "\(numerodeDias) \(diarias) - \(numeroDeHospedes) \(hospedes)"
+        }
+        
+        precoSemDescontoLabel.text = "R$ \(viagem?.precoSemDesconto ?? 0.00)"
+        
+        
+    }
 }
